e12c370e88014069aa42491f21ed25ee52332ac8
Vishnunair9497 <Vishnu.nair1@ust.com>
2025-08-11 19:42:03 +0530

    commited the NotificationHelper.kt changes

--- app/src/main/java/com/example/templepocforground/helper/PubSubServiceActionEnum.kt
+++ app/src/main/java/com/example/templepocforground/helper/PubSubServiceActionEnum.kt
@@ -4,8 +4,6 @@ enum class PubSubServiceActionEnum (val action: String) {
     START_ALERT("START_ALERT"),
     STOP_ALERT("STOP_ALERT"),
     MUTE_ALERT("MUTE_ALERT"),
-    OPEN_APP("OPEN_APP"),
-
     OPEN("ACTION_OPEN"),
     CLOSE("ACTION_CLOSE"),
     MUTE("ACTION_MUTE")
